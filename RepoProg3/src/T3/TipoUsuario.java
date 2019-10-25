01e0cb91dd82dc434a93e0c0635db5a5ca62a798
Haizea <Haizea@10.164.54.8>
2019-10-25 08:50:34 +0200

    Ejemplo fichero T3 + test

---
+++ RepoProg3/src/T3/TipoUsuario.java
@@ -0,0 +1,9 @@
+package T3;
+
+/** Tipo de los usuarios
+ * @author Andoni Eguíluz Morán
+ * Facultad de Ingeniería - Universidad de Deusto
+ */
+public enum TipoUsuario {
+	Admin, Empleado, Cliente, Invitado
+}
